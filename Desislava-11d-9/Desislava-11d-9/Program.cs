a44a4fadaf3e44352b05e6b066c74fd48ac98ca0
DesislavaTsocheva <desislava_tsocheva@abv.bg>
2023-05-26 20:10:39 +0300

    FirstCommit

---
+++ Desislava-11d-9/Desislava-11d-9/Program.cs
@@ -0,0 +1,20 @@
+﻿using Desislava_11d_9.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desislava_11d_9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
